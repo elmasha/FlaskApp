8f4b8c6c6de3d1c65ad03d8e1a71b49cf5e9d783
elmasha <elmashavitran@gmail.com>
2022-01-22 15:45:57 +0300

    Add Heroku deployment files

---
+++ data.py
@@ -0,0 +1,53 @@
+def Articles():
+    articles = [
+        {
+            'id':0,
+            'title': 'Article',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto sunt consequuntur voluptatibus ullam rem fugit dicta consectetur repellat ut, eligendi accusantium excepturi, impedit iure quidem? Dolorum quo laudantium reprehenderit rem!',
+            'author': 'Allan fikro',
+            'date': '02/11/2022'
+        },
+        {
+            'id':1,
+            'title': 'Article one',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto sunt consequuntur voluptatibus ullam rem fugit dicta consectetur repellat ut, eligendi accusantium excepturi, impedit iure quidem? Dolorum quo laudantium reprehenderit rem!',
+            'author': 'elmasha omondi',
+            'date': '02/21/2022'
+        },
+        {
+            'id':2,
+            'title': 'Article two',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto sunt consequuntur voluptatibus ullam rem fugit dicta consectetur repellat ut, eligendi accusantium excepturi, impedit iure quidem? Dolorum quo laudantium reprehenderit rem!',
+            'author': 'alex korir',
+            'date': '02/05/2022'
+        },
+        {
+            'id':3,
+            'title': 'Article three',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto sunt consequuntur voluptatibus ullam rem fugit dicta consectetur repellat ut, eligendi accusantium excepturi, impedit iure quidem? Dolorum quo laudantium reprehenderit rem!',
+            'author': 'joy kanini',
+            'date': '02/16/2022'
+        },
+        {
+            'id':4,
+            'title': 'Article four',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto sunt consequuntur voluptatibus ullam rem fugit dicta consectetur repellat ut, eligendi accusantium excepturi, impedit iure quidem? Dolorum quo laudantium reprehenderit rem!',
+            'author': 'adrian lolly',
+            'date': '02/21/2022'
+        },
+        {
+            'id':5,
+            'title': 'Article five',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto sunt consequuntur voluptatibus ullam rem fugit dicta consectetur repellat ut, eligendi accusantium excepturi, impedit iure quidem? Dolorum quo laudantium reprehenderit rem!',
+            'author': 'lisa liko',
+            'date': '02/05/2022'
+        },
+        {
+            'id':6,
+            'title': 'Article six',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto sunt consequuntur voluptatibus ullam rem fugit dicta consectetur repellat ut, eligendi accusantium excepturi, impedit iure quidem? Dolorum quo laudantium reprehenderit rem!',
+            'author': 'john doe',
+            'date': '02/13/2022'
+        },
+    ]
+    return articles
